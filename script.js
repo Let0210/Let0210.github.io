657257595dcb333576b8ba0eddd618f9b4e54c72
Ana Letícia Rodrigues De Oliveira <103615867+Let0210@users.noreply.github.com>
2023-12-05 22:46:06 -0300

    upload do site info mvp

---
+++ script.js
@@ -0,0 +1,5 @@
+// You can add JavaScript functionality here if needed
+// For instance, dynamically changing the video source
+// Example:
+const newVideoSrc = "NEW_VIDEO_URL";
+document.getElementById('videoFrame').src = newVideoSrc;
