1fb97b0ec093ffd20213feeb57d03bd3d25339cb
Ana Letícia Rodrigues De Oliveira <103615867+Let0210@users.noreply.github.com>
2023-12-13 22:12:00 -0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// You can add JavaScript functionality here if needed
-// For instance, dynamically changing the video source
-// Example:
-const newVideoSrc = "NEW_VIDEO_URL";
-document.getElementById('videoFrame').src = newVideoSrc;
